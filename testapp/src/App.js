4bbd4c1573104821857febf474f2fb3093d14b59
AshwiniTengale <103949995+AshwiniTengale@users.noreply.github.com>
2023-07-16 20:03:33 +0530

    counter component

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
 import Message from './Components/Message';
+import Counter from './Components/Counter';
+
 function App() {
   return (
     <div className="App">
-         
+         <Counter></Counter>
          <Message></Message>
 
 
